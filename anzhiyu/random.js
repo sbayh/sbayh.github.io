4f41676b7743be4366b882910628fac0952b7f46
sbayh <lingeriji@qq.com>
2025-08-14 19:47:05 +0800

    Site updated: 2025-08-14 19:47:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/14/python及pycharm的安装和激活/","2025/08/14/hexo的安装及部署/","2025/08/13/hello-world/"];function toRandomPost(){
+var posts=["2025/08/13/hello-world/","2025/08/14/python及pycharm的安装和激活/","2025/08/14/hexo的安装及部署/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
